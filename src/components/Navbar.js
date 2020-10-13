5472a892702a611c6fd4fceed18771ba78df2625
Emilie <martelemilie@hotmail.fr>
2020-10-13 16:15:02 +0200

    Fin quête

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar () {
+  return (
+    <nav className="Navbar">
+      <a src="https://fr.wikipedia.org/wiki/Les_Simpson">Simpons Quotes</a>
+    </nav>
+  );
+}
+
+export default Navbar;
